a76672204141b439dbb84c8a2d03e8dec67cff8b
Rusell Dayvox <105234503+RustHub97@users.noreply.github.com>
2022-09-18 22:13:04 +0400

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,56 @@
+from selenium import webdriver
+driver = webdriver.Chrome()
+import time
+import pytest
+
+@pytest.fixture(autouse=True, scope="module")
+def testing():
+   pytest_driver = webdriver_Chrome('B:\\PyCharm\\chromedriver.exe')
+   # Переходим на страницу авторизации
+   pytest_driver.get('http://petfriends.skillfactory.ru/login)
+
+   yield
+
+   pytest_driver.quit()
+
+
+def test_show_my_pets():
+   # Вводим email
+   pytest_driver.find_element_by_id('email').send_keys('rustam.s2012@mail.com')
+   # Вводим пароль
+   pytest.driver.find_element_by_id('pass').send_keys('RUSTAM1997')
+   # Нажимаем на кнопку входа в аккаунт
+   pytest.driver.find_element_by_css_selector('button[type="submit"]').click()
+   # Проверяем, что мы оказались на главной странице пользователя
+   assert pytest.driver.find_element_by_tag_name('h1').text == "PetFriends"
+
+def test
+//div[@class=".col-sm-4 left"]/text()[2]
+//div[@class=".col-sm-4 left"]/h2/following-sibling::text()[1]
+
+statistic_user = driver.find_element_by_xpath('//div[@class=".col-sm-4 left"]')
+assert str(len(list_my_pets)) in statistic_user.text
+
+def test_show_my_pets():
+   # Вводим email
+   pytest.driver.find_element_by_id('email').send_keys('victroia@yamdex.ru')
+   # Вводим пароль
+   pytest.driver.find_element_by_id('pass').send_keys('123abc')
+   # Нажимаем на кнопку входа в аккаунт
+   pytest.driver.find_element_by_css_selector('button[type="submit"]').click()
+   # Проверяем, что мы оказались на главной странице пользователя
+   assert pytest.driver.find_element_by_tag_name('h1').text == "PetFriends"
+
+   # Убедиться, что внутри каждого из элементов есть имя питомца, возраст и вид.
+   images = pytest.driver.find_elements_by_css_selector('.card-deck .card-img-top')
+   names = pytest.driver.find_elements_by_css_selector('.card-deck .card-body .card-title')
+   descriptions = pytest.driver.find_elements_by_css_selector('.card-deck .card-body .card-text')
+
+   for i in range(len(names)):
+      assert images[i].get_attribute('src') != ''
+      assert names[i].text != ''
+      assert descriptions[i].text != ''
+      assert ', ' in descriptions[i]
+      parts = descriptions[i].text.split(", ")
+      assert len(parts[0]) > 0
+      assert len(parts[1]) > 0
